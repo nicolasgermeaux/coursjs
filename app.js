53e1e97888d3142e6281f93186d08438bdf6cfd5
Nicolas Germeaux <nicolas@monastudio.co>
2022-11-10 18:35:44 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function check(){
+    let age = document.querySelector(".age").value
+    
+    if(age<=20){
+        console.log("Tu es un petit bébé");
+    }
+    else{
+        console.log("Tu es déjà grand !")
+    }
+}
